06f9d103782386dc604f7e381b2f115eda8ef602
MotryaVasya <motravasa5@gmail.com>
2025-04-27 17:23:45 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplication22/ConsoleApplication22.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+void MyTestFunc222()
+{
+
+}
+
+int main()
+{	
+	MyTestFunc222();
+	for (size_t i = 0; i < 100; i++)
+	{
+		cout << "Hello world!\n";
+	}
+}
+
